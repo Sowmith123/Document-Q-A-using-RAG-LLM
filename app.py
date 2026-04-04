6679eb51c4c9b3ca73e9b4bb4dd13df69b955147
P SOWMITH <135353249+Sowmith123@users.noreply.github.com>
2026-04-04 11:22:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.chains import RetrievalQA
+from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain_openai import ChatOpenAI
 import tempfile
 import os
